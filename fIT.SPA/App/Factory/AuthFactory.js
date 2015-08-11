22fe0a5ff595181ccfc3c264449e18493b802a71
Fanuer <SteSue@web.de>
2015-08-11 16:29:44 +0200

    added SPA

---
+++ fIT.SPA/App/Factory/AuthFactory.js
@@ -0,0 +1,71 @@
+﻿function AuthFactory($http, $q, $log, localStorageService, baseUrl) {
+  var factory = {};
+  var _authentication = {
+    isAuth: false,
+    userName: ""
+  }
+
+  var _register = function(registrationModel) {
+    _logout();
+    return $http.post(baseUrl + '/api/account/register', registrationModel)
+      .then(function(response) {
+        return response;
+      })
+      .catch(function(response) {
+      $log.error('Error: ' + response.statusCode);
+    });
+  }
+
+  var _login = function (loginData) {
+    var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+    var deferred = $q.defer();
+    $http.post(baseUrl + '/api/account/login', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+      .success(function (response) {
+      localStorageService.set('authorizationData', {
+        token: response.access_token, 
+        userName: loginData.userName, 
+        expireDate: new Date(response['.expires']),
+        refreshToken: response.refresh_token
+      });
+
+      _authentication.isAuth = true;
+      _authentication.userName = loginData.userName;
+
+      deferred.resolve(response);
+
+      })
+      .error(function (err, status) {
+      _logOut();
+      deferred.reject(err);
+    });
+
+    return deferred.promise;
+
+  };
+
+  var _logOut = function () {
+
+    localStorageService.remove('authorizationData');
+    _authentication.isAuth = false;
+    _authentication.userName = "";
+
+  };
+
+  var _fillAuthData = function () {
+
+    var authData = localStorageService.get('authorizationData');
+    if (authData) {
+      _authentication.isAuth = true;
+      _authentication.userName = authData.userName;
+    }
+  }
+
+  factory.saveRegistration = _register;
+  factory.login = _login;
+  factory.logOut = _logOut;
+  factory.fillAuthData = _fillAuthData;
+  factory.authentication = _authentication;
+
+
+  return factory;
+}

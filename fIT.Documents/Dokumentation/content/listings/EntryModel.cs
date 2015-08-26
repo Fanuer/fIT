fa1154d9afd46a315eac8651e86adf2d899092d6
Fanuer <SteSue@web.de>
2015-08-26 16:28:59 +0200

    DB Doku

---
+++ fIT.Documents/Dokumentation/content/listings/EntryModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fIT.WebApi.Models
+{
+    /// <summary>
+    /// Defines one entry from the server
+    /// </summary>
+    /// <typeparam name="T">Type of id-property</typeparam>
+    public class EntryModel<T>
+    {
+        /// <summary>
+        /// Id of an entity
+        /// </summary>
+        [Required(ErrorMessageResourceName = "Error_Required", ErrorMessageResourceType = typeof(Resources))]
+        [Display(ResourceType = typeof(Resources), Name = "Label_Id")]
+        public T Id { get; set; }
+
+        /// <summary>
+        /// Name of an Entity
+        /// </summary>
+        [Required(ErrorMessageResourceName = "Error_Required", ErrorMessageResourceType = typeof(Resources))]
+        [Display(ResourceType = typeof(Resources), Name = "Label_Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Url to receive this entity
+        /// </summary>
+        [Display(ResourceType = typeof(Resources), Name = "Label_Url")]
+        public string Url { get; set; }
+    }
+}

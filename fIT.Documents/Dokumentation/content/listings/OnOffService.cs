f1c73035481b0628eaebc041c6e3921a73b5dc91
Kevin Schie <kevinschie@Kevins-MBP.fritz.box>
2015-08-30 19:24:17 +0200

    Kapitel fitNat fürs Erste fertig. Quellen fehlen noch und ein Kampf mit den Bildern auch...

---
+++ fIT.Documents/Dokumentation/content/listings/OnOffService.cs
@@ -0,0 +1,39 @@
+public async Task<Guid> SignIn(string username, string password)
+{
+    User user = new fITNat.DBModels.User();
+    Guid userId;
+    user.Username = username;
+    user.Password = password;
+    if (Online)
+    {
+        try
+        {
+            bool success = await mgnService.SignIn(username, password);
+            if (success)
+            {
+                userId = mgnService.actualSession().CurrentUserId;
+                user.UserId = userId.ToString();
+                if (db != null)
+                {
+                    db.insertUpdateUser(user);
+                }
+                return userId;
+            }
+        }
+        catch(ServerException ex){[...]throw;}
+        catch(Exception exc){[...]}
+        return new Guid();
+    }
+    else
+    {
+        try
+        {
+            //Lokal nachgucken
+            Guid result = db.findUser(username, password);
+            if (result != null)
+                return result;
+        }
+        catch(Exception exc){[...]throw;}
+        return new Guid();
+    }
+}
